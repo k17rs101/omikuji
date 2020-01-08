3890e5a7808661e9e7b38994810e0abf7b73c6f7
k17rs101 <k17rs101@st.kyusan-u.ac.jp>
2020-01-08 12:59:18 +0900

    凶の追加

--- www/js/ncmbController.js
+++ www/js/ncmbController.js
@@ -7,7 +7,29 @@ var ncmbController = {
     currentUser: null,  // ログインしたユーザーのオブジェクトを格納
     screenSize: null,    // 画面サイズを格納
 
-    // 初期化
+
+
+    // スコア送信
+    sendScore: function(dice) {
+      var self = this;
+
+      // [1]Score（クラス）を生成
+      var Score = self.ncmb.DataStore("OmikujiClass");
+
+      // [2]インスタンス生成、スコア数値をフィールド名"score"にセット
+      var scoreData = new Score({dice: dice});
+
+      // [3]送信処理
+      scoreData.save()
+          .then(function (saved) {
+              alert("運勢送信完了！");
+          })
+        .catch(function(err){
+              console.log(err);
+          });
+          
+  },
+      // 初期化
     init: function(screenSize) {
         var self = this;
         self.ncmb = new NCMB(self.APPLICATION_KEY, self.CLIENT_KEY);    // mobile backendの初期化
